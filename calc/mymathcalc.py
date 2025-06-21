ac3a1a26d01a134fa130574ad65e8a8799fca537
Subrato Mondal <subratomondal374@gmail.com>
2025-06-22 01:38:21 +0530

    pyhton concepts

---
+++ calc/mymathcalc.py
@@ -0,0 +1,16 @@
+"""
+Date: 19-6-2025
+Author: Subrato
+Desc: Understanding module using these functions
+"""
+
+def area_of_circle(rad):
+    return 3.14* rad*rad
+
+def circuference_of_circle(rad):
+    return 2*3.14* rad
+
+def area_of_sq(side):
+    return side*side
+
+
