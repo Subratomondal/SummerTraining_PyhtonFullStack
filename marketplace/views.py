dfaee3080616736b2961b94da12eda1267fe4cee
Subrato Mondal <119128874+Subratomondal@users.noreply.github.com>
2025-06-25 13:58:21 +0530

    Delete marketplace directory

--- marketplace/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-def homepage(request):
-    return HttpResponse("<h1>Welcome to Craft Nest - Artisans Meet Buyers</h1>")
